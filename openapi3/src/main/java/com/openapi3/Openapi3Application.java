caba12550e4dff970fee0b649d03a4507c79ba53
ZTHQWQ <117910386+ZTHQWQ@users.noreply.github.com>
2025-06-21 21:51:16 +0800

    OpenAPI 3.0

--- openapi3/src/main/java/com/openapi3/Openapi3Application.java
+++ openapi3/src/main/java/com/openapi3/Openapi3Application.java
@@ -1,7 +1,11 @@
 package com.openapi3;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Openapi3Application {
@@ -9,5 +13,19 @@ public class Openapi3Application {
     public static void main(String[] args) {
         SpringApplication.run(Openapi3Application.class, args);
     }
+
+    @Bean
+    public OpenAPI myOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("社区交互系统 API 文档")
+                        .version("1.0.0")
+                        .description("基于 Spring Boot 的后端接口文档")
+                        .contact(new Contact()
+                                .name("张三")
+                                .email("zhangsan@example.com")
+                                .url("https://yourdomain.com")));
+    }
+
 
 }
